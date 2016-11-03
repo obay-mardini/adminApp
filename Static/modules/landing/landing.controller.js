f594ba93777f86662e6d0863bfe177777db010fb
obay <obaymardini@gmail.com>
2016-11-03 21:46:45 +0100

    add countries information to the map

--- Static/modules/landing/landing.controller.js
+++ Static/modules/landing/landing.controller.js
@@ -18,16 +18,49 @@
         vm.draw = settingTheMap;
         vm.usersByCountry = {};
         
+//        function settingTheMap() {
+//            var numberOfUsers = 0;
+//            queryDb.queryUsers('/queryUsers').then(function(result) {
+//                vm.usersByCountry = {};
+//                result.forEach(function(user) {
+//                    vm.usersByCountry[user.country] = vm.usersByCountry[user.country] || 0;
+//                    vm.usersByCountry[user.country]++;
+//                    numberOfUsers++;
+//                });
+//                infoMap.draw(vm.usersByCountry, numberOfUsers, usersBased)
+//            }).catch(function(err) {
+//                console.log(err)
+//            })
+//        }
+        
         function settingTheMap() {
             var numberOfUsers = 0;
-            queryDb.queryUsers('/queryUsers').then(function(result) {
-                vm.usersByCountry = {};
-                result.forEach(function(user) {
-                    vm.usersByCountry[user.country] = vm.usersByCountry[user.country] || 0;
-                    vm.usersByCountry[user.country]++;
-                    numberOfUsers++;
-                });
-                infoMap.draw(vm.usersByCountry, numberOfUsers)
+            queryDb.queryUsers('/queryJournies').then(function(result) {
+//                vm.usersByCountry = {};
+//                result.forEach(function(user) {
+//                    vm.usersByCountry[user.country] = vm.usersByCountry[user.country] || 0;
+//                    vm.usersByCountry[user.country]++;
+//                    numberOfUsers++;
+//                });
+                
+                function destinationBased(d, i) {
+                    var destionations = result.reduce(function(target, current){
+                    target[current.destinationcountry] = target[current.destinationcountry] || 0;
+                    target[current.destinationcountry]++;
+                    return target;
+                    }, {})
+                    if (destionations[d.properties.name] / result.length > 0.2) {
+                        return "blue";
+                    } else if (destionations[d.properties.name] / result.length > 0.1) {
+                        return 'red'
+                    } else {;
+                        return 'green';
+                    }
+
+                    return 'black';
+                }
+                
+                infoMap.draw(vm.usersByCountry, null, destinationBased)
             }).catch(function(err) {
                 console.log(err)
             })
