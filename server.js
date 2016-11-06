a0661717dcf6456ade29c5a64c03fef3176bbedc
obay <obaymardini@gmail.com>
2016-11-06 22:42:24 +0100

    clients pooling

--- server.js
+++ server.js
@@ -5,32 +5,23 @@ var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 var querystring = require('querystring');
-var url = require('url');
-//var session = require('express-session');
+URL = require("url-parse");
 var dbUrl = process.env.DATABASE_URL || "postgres://spiced:spiced1@localhost:5432/bookMe";
-//var configSession = require('connect-redis')(session);
-var redis = require('redis');
-var client = redis.createClient({
-    host: 'localhost',
-    port: 6379
-});
-
 var n =1;
+var x = new URL("postgres://spiced:spiced1@localhost:5432/bookMe")
+console.log(x)
 
-client.on('error', function(err) {
-    console.log(err);
-});
-
-//app.use(session({
-//    store: new configSession({
-//        ttl: 3600,
-//        host: 'localhost',
-//        port: 6379
-//    }),
-//    resave: false,
-//    saveUninitialized: true,
-//    secret: 'nothing in my mind now'
-//}));
+var herokuDb = new URL(process.env.DATABASE_URL) || null;
+var config = {
+  user: herokuDb.username || 'spiced', 
+  database: herokuDb.path || 'bookMe', 
+  password: herokuDb.password || 'spiced1', 
+  host: 'localhost', 
+  port: 5432,
+  max: 10, 
+  idleTimeoutMillis: 30000
+};
+var pool = new pg.Pool(config);
 
 app.use(express.static(__dirname + '/Static'));
 
@@ -53,24 +44,30 @@ var isAdmin = function(req,res,next){
     }
 app.get('/queryUsers', function(req, res, next) {
     n += 1;
-    console.log(n)
-    var client = new pg.Client(dbUrl);
-    client.connect(function(err){
-        if(err){
-            console.log(err)
+    console.log(n);
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
         }
-        
-        var query = "SELECT * from users";
-        
-        client.query(query, [], function(error,result) {
-                if(error){
-                    console.log(error);
-                } else {
-                    res.json(result.rows);
-                }
+        var client = new pg.Client(dbUrl);
+//        client.connect(function(err){
+//            if(err){
+//                console.log(err)
+//            }
+
+            var query = "SELECT * from users";
+
+            client.query(query, [], function(error,result) {
+                    if(error){
+                        console.log(error);
+                    } else {
+                        res.json(result.rows);
+                        done();
+                    }
 
             });
-    })
+//        })
+    });
 
 });
 
